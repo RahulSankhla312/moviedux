c52098e11ed29bb1214f7f5fd02dce93691698fa
RahulSankhla312 <rahulsankhla312@gmail.com>
2025-01-18 14:24:25 +0530

    Added the movie cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import './App.css'
 import './styles.css'
 import Header from './components/Header';
 import Footer from './components/Footer';
+import MoviesGrid from './components/MoviesGrid';
 
 function App() {
 
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
       <div className='container' >
         <Header> </Header>
+        <MoviesGrid> </MoviesGrid>
       </div>
       <Footer>  </Footer>
     </div>
