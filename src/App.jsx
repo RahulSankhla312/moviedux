5e36f641ca3f666bfde022f68a7e784c69dfb9f3
RahulSankhla312 <rahulsankhla312@gmail.com>
2025-01-19 01:19:55 +0530

    Routing Started

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
-import './styles.css'
-import Header from './components/Header';
-import Footer from './components/Footer';
-import MoviesGrid from './components/MoviesGrid';
+import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MoviesGrid from "./components/MoviesGrid";
+import Watchlist from "./components/Watchlist";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-
   return (
     <div className="App">
-      <div className='container' >
+      <div className="container">
         <Header> </Header>
-        <MoviesGrid> </MoviesGrid>
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<MoviesGrid />} />
+            <Route path="/watchlist" element={<Watchlist />} />
+          </Routes>
+        </Router>
       </div>
-      <Footer>  </Footer>
+      <Footer> </Footer>
     </div>
   );
 }
 
-export default App
+export default App;
